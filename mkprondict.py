069ce557549f495547d8e597fdfed1943a9fc649
Camille Goudeseune <cog@illinois.edu>
2018-04-12 15:10:04 -0500

    variable names

--- mkprondict.py
+++ mkprondict.py
@@ -10,7 +10,7 @@ import re
 
 USAGE='''USAGE: mkprondict.py in.txt in.dict out.txt out.dict words.txt phones.txt missing.txt
 
-   Clean the words in in.txt: keep only the characters in in.dict, and eliminate >2 repeated chars.
+   Clean the words from in.txt: keep only the characters in in.dict, and eliminate >2 repeated chars.
    Write cleaned text into out.txt; write the prondict into out.dict.
    Write the word list and phone list into words.txt and phones.txt.
    Into missing.txt, report any characters in in.txt that were not in in.dict.'''
@@ -19,88 +19,82 @@ if len(sys.argv) != 8:
     print(USAGE)
     exit(0)
 
-intxt = sys.argv[1]
-indict = sys.argv[2]
-outtxt = sys.argv[3]
-outdict = sys.argv[4]
-words = sys.argv[5]
-phones = sys.argv[6]
-missingchars = sys.argv[7]
+dummy, fileIntxt, fileIndict, fileOuttxt, fileOutdict, fileWords, filePhones, fileMissingchars = sys.argv
 
-# Read indict.
-g2p = [{},{},{},{}];
+# Read fileIndict.
+g2p = [];
 phoneset = {}
-with open(indict) as f:
+with open(fileIndict) as f:
     for line in f:
-        f=line.rstrip().split()
-        if len(f)>1:
-            n = len(f[0])-1
+        words = line.rstrip().split()
+        if len(words) > 1:
+            n = len(words[0]) - 1
             while n >= len(g2p):
                 g2p.append({})
-            g2p[n][f[0].upper()] = ' '.join(f[1:])
-            for p in f[1:]:
+            g2p[n][words[0].upper()] = ' '.join(words[1:])
+            for p in words[1:]:
                 phoneset[p] = 1
 
-# Read intxt, convert words, and write outtxt.
+# Read fileIntxt, convert words, and write fileOuttxt.
 prondict = {}
 missingchars = {}
-with open(intxt) as f:
-    with open(outtxt, 'w') as g:
-        print('Cleaning text from %s into %s.' % (intxt, outtxt))
+with open(fileIntxt) as f:
+    with open(fileOuttxt, 'w') as g:
+        print('Cleaning text from %s into %s.' % (fileIntxt, fileOuttxt))
         for line in f:
-            words = re.split('\W+', line)
             outwords = []
-            for word in words:
-                if word.upper() in prondict:
-                    outwords.append(word.upper())
+            for word in re.split('\W+', line.upper()):
+                if word in prondict:
+                    outwords.append(test)
                 else:
                     rec = []
                     pron = ''
-                    test = word.upper()
-                    while (len(test)>0):
+                    while word:
                         # Find the longest-matching char sequence.
-                        for n in range(min(len(test), len(g2p)), -1, -1):
-                            if n==0:
+                        for n in range(min(len(word), len(g2p)), -1, -1):
+                            if n == 0:
                                 # Character missing, so delete it from cleaned output.
-                                missingchars[test[0]] = 1
-                                test = test[1:]
+                                missingchars[word[0]] = 1
+                                word = word[1:]
                                 break
-                            elif test[0:n] in g2p[n-1]:
+                            elif word[0:n] in g2p[n-1]:
                                 # Keep this to cleaned output only if it's not a triple-repetition.
-                                if len(rec)<2 or test[0:n] != rec[-1] or rec[-1] != rec[-2]:
-                                    rec.append(test[0:n])
-                                    pron += ' '+g2p[n-1][test[0:n]]
-                                test = test[n:]
+                                if len(rec)<2 or word[0:n] != rec[-1] or rec[-1] != rec[-2]:
+                                    rec.append(word[0:n])
+                                    pron += ' ' + g2p[n-1][word[0:n]]
+                                word = word[n:]
                                 break
-                    if len(rec)>0:
+                    if rec:
                         prondict[''.join(rec)] = pron
                         outwords.append(''.join(rec))
-            if len(outwords)>0:
-                g.write(' '.join(outwords)+'\n')
+            if outwords:
+                g.write(' '.join(outwords) + '\n')
 
 # Eliminate the unwanted blank word.
 if '' in prondict:
     del prondict['']
     
 # Write the dictionary.
-with open(outdict, 'w') as f:
-    print('Writing {}'.format(outdict))
+with open(fileOutdict, 'w') as f:
+    print('Writing {}'.format(fileOutdict))
     for (k,v) in sorted(prondict.items()):
-        if len(k) > 0:
+        if k:
             f.write('%s %s\n' % (k,v))
 
 # Write the list of missing chars.
-with open(missingchars, 'w') as f:
+with open(fileMissingchars, 'w') as f:
     print('Writing {}'.format(missingchars))
     for k in missingchars.keys():
-        f.write(k+'\n')
+        f.write(k + '\n')
         
-# Write the list of words and the list of phones.
-with open(words, 'w') as f:
-    print('Writing {}'.format(words))
+# Write the list of words.
+with open(fileWords, 'w') as f:
+    print('Writing {}'.format(fileWords))
     for (n,w) in enumerate(sorted(prondict.keys())):
         f.write('{}\n'.format(w))
-phones = {}
-with open(phones, 'w') as f:
+
+# Write the list of phones.
+with open(filePhones, 'w') as f:
+    print('Writing {}'.format(filePhones))
     for (n,p) in enumerate(sorted(phoneset.keys())):
         f.write('{}\n'.format(p))
