9afcd9d0f78b0f69af0579254459c89717d09a5a
Camille Goudeseune <cog@illinois.edu>
2018-06-20 15:33:44 -0500

    remove duplicate rules in g2p's

--- make-g2ps.rb
+++ make-g2ps.rb
@@ -12,7 +12,7 @@ Dir.glob(G2PS + '/*/*_wikipedia_symboltable.txt') {|f|
     next
   end
   $fileErr = $out + '/' + File.basename(f, 'txt') + 'err'
-  `./g2ipa2asr.py #{f} aspire2ipa.txt phoibletable.csv > #$out/#{File.basename(f)} 2> #$fileErr`
+  `./g2ipa2asr.py #{f} aspire2ipa.txt phoibletable.csv 2> #$fileErr | sort -u > #$out/#{File.basename(f)}`
   $err = `sort -u < #$fileErr`
   puts File.basename(f) + ":\n" + $err + "\n" if !$err.empty?
 }
