b6bd164f146e524a2c3b666080ce435e7f0364b8
Camille Goudeseune <cog@illinois.edu>
2018-06-25 15:07:53 -0500

    fileInwords is already clean

--- mkprondict_from_wordlist.py
+++ mkprondict_from_wordlist.py
@@ -20,10 +20,9 @@ fileOutdict = L + "/local/dict/lexicon.txt" # fileOuttxt's words, each with a pr
 
 # Extra outputs.
 filePhones = "/tmp/phones.txt"          # fileOuttxt's phones, a subset of L/local/dict/nonsilence_phones.txt, which is the standard Aspire version.
-fileMissingchars = "/tmp/letters-culled-by-cleaning.txt"    # Any characters in fileIntxt that were not in fileIndict.
 
 # Make dirs of output files.
-for filename in [fileOutdict, fileMissingchars, filePhones]:
+for filename in [fileOutdict, filePhones]:
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
 # Read fileIndict.
@@ -55,50 +54,39 @@ if phonesBogus:
     print('Invalid phones in ' + fileIndict + ': ' + str(phonesBogus))
     exit(1)
 
-# Read fileInwords, convert words
+# Read fileInwords.  Convert words.
 prondict = {}
-missingchars = {}
 with open(fileInwords, 'r', encoding='utf-8') as f:
     print('Making prondict from %s.' % fileInwords)
     for line in f:
-        # Assume one word per line; strip off newline, and uppercase it
-        word=line.rstrip().upper()
+        # Assume one word per line.  Strip off newline, and uppercase it.
+        word = line.rstrip().upper()
+        if not word:
+            continue
         if word in prondict:
-            # This word already appeared in fileInwords.
-            pass
-        elif word:
-            rec = []
-            pron = ''
-            while word:
-                # Find the longest-matching char sequence.
-                for n in range(min(len(word), len(g2p)), -1, -1):
-                    if n == 0:
-                        # Character missing, so delete it from cleaned output.
-                        missingchars[word[0]] = 1
-                        word = word[1:]
-                        break
-                    elif word[0:n] in g2p[n-1]:
-                        # Keep this to cleaned output only if it's not a triple-repetition.
-                        if len(rec)<2 or word[0:n] != rec[-1] or rec[-1] != rec[-2]:
-                            rec.append(word[0:n])
-                            pron += ' ' + g2p[n-1][word[0:n]]
-                        word = word[n:]
-                        break
-            if rec:
-                rec = ''.join(rec)
-                prondict[rec] = pron
+            print('Invalid duplicate word ' + word + ' in cleaned wordlist ' + fileInwords)
+            exit(1)
+        rec = []
+        pron = ''
+        while word:
+            # Find the longest-matching char sequence.
+            for n in range(min(len(word), len(g2p)), -1, -1):
+                if n == 0:
+                    print('Invalid char "' + word[0] + '" in cleaned wordlist ' + fileInwords)
+                    exit(1)
+                elif word[0:n] in g2p[n-1]:
+                    rec.append(word[0:n])
+                    pron += ' ' + g2p[n-1][word[0:n]]
+                    word = word[n:]
+                    break
+        if rec:
+            prondict[''.join(rec)] = pron
 
 # Write the dictionary.
 with open(fileOutdict, 'w', encoding='utf-8') as f:
     print('Writing {}'.format(fileOutdict))
     for (k,v) in sorted(prondict.items()):
         f.write('%s %s\n' % (k,v))
-
-# Write the list of missing chars.
-with open(fileMissingchars, 'w', encoding='utf-8') as f:
-    print('Writing {}'.format(fileMissingchars))
-    for k in sorted(missingchars.keys()):
-        f.write(k + '\n')
 
 # Write the list of phones.
 with open(filePhones, 'w') as f:
