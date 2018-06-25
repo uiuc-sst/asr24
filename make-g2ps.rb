c07c056a5b5f4d4c5e087e447a784a33add0c7da
Camille Goudeseune <cog@illinois.edu>
2018-06-25 09:52:28 -0500

    use uiuc-sst/g2ps.git directly

--- make-g2ps.rb
+++ make-g2ps.rb
@@ -1,10 +1,18 @@
 #!/usr/bin/env ruby
 
 # Convert IPA g2p's to Aspire g2p's.
+# This takes 10 seconds.
 
-G2PS = '/tmp/g2ps' # In the parent dir (/tmp), do: git clone https://github.com/uiuc-sst/g2ps.git
+G2PS = '/tmp/g2ps'
+if !Dir.exist?(G2PS)
+  # This takes 15 seconds.
+  `cd #{File.dirname(G2PS)} && git clone https://github.com/uiuc-sst/g2ps.git`
+else
+  `cd #{G2PS} && git pull`
+end
+
 $out = '/tmp/g2ps-aspire'
-`mkdir -p #$out`
+`mkdir -p #$out; rm -rf #$out/*`
 
 Dir.glob(G2PS + '/*/*_wikipedia_symboltable.txt') {|f|
   if File.basename(f) == 'Thai_wikipedia_symboltable.txt'
@@ -12,7 +20,11 @@ Dir.glob(G2PS + '/*/*_wikipedia_symboltable.txt') {|f|
     next
   end
   $fileErr = $out + '/' + File.basename(f, 'txt') + 'err'
-  `./g2ipa2asr.py #{f} aspire2ipa.txt phoibletable.csv 2> #$fileErr | sort -u > #$out/#{File.basename(f)}`
+  `./g2ipa2asr.py #{f} aspire2ipa.txt #{G2PS}/_config/phoibletable.csv 2> #$fileErr | sort -u > #$out/#{File.basename(f)}`
   $err = `sort -u < #$fileErr`
-  puts File.basename(f) + ":\n" + $err + "\n" if !$err.empty?
+  if $err.empty?
+    File.delete $fileErr
+  else
+    puts File.basename(f) + ":\n" + $err + "\n"
+  end
 }
