01481f14acf4f49633fe771e20865f3b4ebfb31c
Camille Goudeseune <cog@illinois.edu>
2019-04-16 16:41:26 -0500

    refactor for loadbalancing

--- mkscp.rb
+++ mkscp.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-# Port of mkscp.py.
-
 # Split a listing of jobs into num_jobs script files and spk2utt files, called
 # lang/scp/\d\d.txt, lang/cmd/\d\d.sh, and lang/spk2utt/\d\d.txt.
 # From dirWav, use only the .wav files; ignore other files.
 
 require 'fileutils'
 
-Usage = "USAGE: mkscp.py wav_dir num_jobs lang_dir"
+Usage = "USAGE: #$0 wav_dir num_jobs lang_dir"
 if ARGV.size != 3 || ARGV[1] !~ /^\d+$/
   STDERR.puts Usage
   exit 1
@@ -64,29 +62,39 @@ $basic_cmd = "online2-wav-nnet3-latgen-faster --online=false --frame-subsampling
   --acoustic-scale=1.0 --word-symbol-table=#$lang/graph/words.txt \
   exp/tdnn_7b_chain_online/final.mdl #$lang/graph/HCLG.fst"
 
+# Distribute indexes of .wav's into $numJobs bins, naively for now.
+$bins = Array.new($numJobs) {[]}
+$numJobs.times {|i|
+  ((i*$num_per_job).ceil ... [((i+1)*$num_per_job).ceil, $wavs.size].min).each {|m|
+    $bins[i] << m
+  }
+}
+$bins.freeze
+
 $cmd_submit = $lang + '-submit.sh'
 File.open($cmd_submit, "w") {|j|
   j.puts "#!/usr/bin/env bash\nrm -rf #$lat_base; mkdir #$lat_base"
-  $numJobs.times {|n|
-    $scpfilename     =     "#$scp_base#{'%2.2d' % n}.txt"
-    $spk2uttfilename = "#$spk2utt_base#{'%2.2d' % n}.txt"
+  $numJobs.times {|i|
+    $i = '%2.2d' % i
+    $scpfilename     =     "#$scp_base#$i.txt"
+    $spk2uttfilename = "#$spk2utt_base#$i.txt"
+
     File.open($scpfilename, "w") {|f|
-      File.open($spk2uttfilename, "w") {|g|
-	((n*$num_per_job).ceil ... [((n+1)*$num_per_job).ceil, $wavs.size].min).each {|m|
-	  f.puts "#{$ids[m]}\t#$dirWav/#{$wavs[m]}"
-	  g.puts "#{$ids[m]}\t#{$ids[m]}"
-	}
-      }
+      $bins[i].each {|m| f.puts "#{$ids[m]}\t#$dirWav/#{$wavs[m]}"}
     }
-    $latfilename   = "#$lat_base#{'%2.2d' % n}.lat"
-    $nbestfilename = "#$lat_base#{'%2.2d' % n}.nbest"
-#   $wordsfilename = "#$lat_base#{'%2.2d' % n}.words"
-#   $asciifilename = "#$lat_base#{'%2.2d' % n}.ascii"
-    $logfilename   = "#$lat_base#{'%2.2d' % n}.log"
-    $cmdfilename   = "#$cmd_base#{'%2.2d' % n}.sh"
+    File.open($spk2uttfilename, "w") {|f|
+      $bins[i].each {|m| f.puts "#{$ids[m]}\t#{$ids[m]}"}
+    }
+
+#   $wordsfilename = "#$lat_base#$i.words"
+#   $asciifilename = "#$lat_base#$i.ascii"
+    $latfilename   = "#$lat_base#$i.lat"
+    $nbestfilename = "#$lat_base#$i.nbest"
+    $logfilename   = "#$lat_base#$i.log"
+    $cmdfilename   = "#$cmd_base#$i.sh"
     File.open($cmdfilename, "w") {|h|
-      h.puts ". cmd.sh\n. path.sh\n"
-      h.puts "module unload gcc/4.7.1 gcc/4.9.2\nmodule load python/2\nmodule swap gcc/6.2.0 gcc/7.2.0\n" if qsub
+      h.puts ". cmd.sh\n. path.sh"
+      h.puts "module unload gcc/4.7.1 gcc/4.9.2\nmodule load python/2\nmodule swap gcc/6.2.0 gcc/7.2.0" if qsub
       h.puts "#$basic_cmd 'ark:#$spk2uttfilename' 'scp:#$scpfilename' 'ark:#$latfilename' 2> #$logfilename"
 #     h.puts "lattice-to-nbest --acoustic-scale=0.1 --n=9 'ark:#$latfilename' 'ark:#$nbestfilename'"
 #     h.puts "nbest-to-linear 'ark:#$nbestfilename' ark:/dev/null 'ark,t:#$wordsfilename ark:/dev/null ark:/dev/null"
@@ -100,4 +108,3 @@ File.open($cmd_submit, "w") {|j|
   j.puts "qstat -u cog" if qsub
 }
 FileUtils.chmod 0775, $cmd_submit
-exit 0
