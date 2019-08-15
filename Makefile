2daee55ddcdfe7ec511f1a7cef805a7a421baf34
Camille Goudeseune <cog@illinois.edu>
2019-08-15 15:50:47 -0500

    dynamic programming; full lcs algorithm

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: lcs
-	./lcs
-lcs: lcs.cpp
-	g++ -std=c++11 -O3 lcs.cpp -o $@
+all: lcs Makefile
+	./lcs < trie1-scrips.txt
+lcs: lcs.cpp Makefile
+	g++ -std=c++11 -O3 -Wall lcs.cpp -o $@
--- lcs.cpp
+++ lcs.cpp
@@ -1,92 +1,55 @@
-// Longest common substring.
-// Compile with g++ -std=c++11.
+// Longest common substring.  (See Makefile.)
 
 #include <algorithm>
+#include <cstring>
 #include <fstream>
 #include <iostream>
-#include <set>
+#include <iterator>
+#include <map>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-// Speedup: replace std::string with e.g. char[50] if that's long enough for all substrings,
-// to avoid many little mallocs.
+// Later, for speed: to avoid mallocs,
+// replace std::string with char[50] if that's long enough for all substrings.
 
-void findSubstrings(const string& word, set<string>& substrings) {
-  // If l > 1000 or so, instead split word (at newlines) into an array, and call this function's sister.
-  const int l = word.length();
-  for (int i=0; i<l; ++i)
-    for (int length = 1; length < l-i+1; ++length)
-      substrings.insert(word.substr(i, length));
-}
-
-void findSubstrings(const vector<string>& words, set<string>& substrings) {
-  // set<>::insert() becomes slow when set::size() > 1e6, and we need over 1e7.
-  // Instead, fill a vector and then remove duplicates, even though that uses more RAM.
-  cout << "Getting " << words.size() << ".\n"; // 133k
-  int j = 0;
-  vector<string> v;
-  for (auto word: words) {
-    ++j; if (j%5000 == 0) { cout << (double)j/words.size() << "\r"; cout.flush(); }
-    const int l = word.size();
-    for (int i=0; i<l; ++i)
-      for (int length = 1; length < l-i+1; ++length)
-	v.emplace_back(word.substr(i, length));
+// Longest common substring algorithm, via dynamic programming.
+// But accumulate only substrings at least as long as lBest,
+// because those are the only ones that the caller would keep.
+vector<int> lcs(const string& a, const string& b, const int lBest=0) {
+  const auto ca = a.size();
+  const auto cb = b.size();
+  int l[ca][cb]; // Lengths of substrings.
+  memset(l, 0, sizeof(l));
+  int lMax = 0;  // Max so far of l[][]'s.
+  vector<int> iBests; // Longest substrings so far, of length lMax, as offsets into "a".
+  //cout << "ca cb = " << ca << " " << cb << "\n";
+  for (auto ia=0u; ia<ca; ++ia)
+  for (auto ib=0u; ib<cb; ++ib) {
+    if (a[ia] != b[ib])
+      continue;
+    // Found a substring.
+    l[ia][ib] = (ia==0 || ib==0) ? 1 : l[ia-1][ib-1] + 1;
+    if (l[ia][ib] > lMax) {
+      // Found a longer substring.  Discard any shorter ones.
+      lMax = l[ia][ib];
+      iBests.clear();
+    }
+    if (lMax >= lBest && l[ia][ib] >= lMax) {
+      // Accumulate the starting offset of another substring
+      // of the same length (or longer: previous "if").
+      // Omit duplicates.
+      const int iNew = ia - lMax + 1;
+      if (iBests.empty() || iBests.back() != iNew)
+	iBests.push_back(iNew);
+    }
   }
-#if 0
-  // This builds 6x faster, but its often-called set_intersection() is much slower.
-  // #include <unordered_set>
-  substrings = unordered_set<string>(make_move_iterator(v.begin()), make_move_iterator(v.end()));
-#else
-  cout << "Sorting " << v.size() << ".\n"; // 41M
-  sort(v.begin(), v.end());
-  cout << "Uniqing.\n";
-  v.erase(unique(v.begin(), v.end()), v.end());
-  cout << "Copying " << v.size() << ".\n"; // 16M
-  substrings = std::set<string>(std::make_move_iterator(v.begin()), std::make_move_iterator(v.end()));
-#endif
-}
-
-struct size_less {
-  template<class T> bool operator()(T const &a, T const &b) const { return a.size() < b.size(); }
-};
-
-// Brute force, but findSubstrings(s2) can be done just once and then used for each line of the prondict.
-//
-// Dynamic programming needs a 2d array of prondictSize * lineSize, 7M * 100, about 7 GB.  Small enough.
-// Actually, smaller.  Run lcs() on each line of s2, collect those results, report the longest one.
-string lcs(const string& s1, const string& s2) {
-  set<string> firstSubstrings, secondSubstrings;
-  cout << "Build substrings for " << s1.size() << " chars.\n";
-  findSubstrings(s1, firstSubstrings);
-  cout << "Build substrings for " << s2.size() << " chars.\n";
-  findSubstrings(s2, secondSubstrings);
-  cout << "Intersecting.\n";
-  set<string> common;
-  set_intersection(firstSubstrings.begin(), firstSubstrings.end(), 
-    secondSubstrings.begin(), secondSubstrings.end(),
-    inserter(common, common.begin()));
-  if (common.empty())
-    return "";
-  return *max_element(common.begin(), common.end(), size_less());
-}
-
-string lcs(const string& s1, const vector<string>& s2) {
-  set<string> firstSubstrings, secondSubstrings;
-  cout << "Build substrings for " << s1.size() << " chars.\n";
-  findSubstrings(s1, firstSubstrings);
-  cout << "Build substrings for " << s2.size() << " words.\n";
-  findSubstrings(s2, secondSubstrings);
-  cout << "Intersecting.\n";
-  set<string> common;
-  set_intersection(firstSubstrings.begin(), firstSubstrings.end(), 
-    secondSubstrings.begin(), secondSubstrings.end(),
-    inserter(common, common.begin()));
-  if (common.empty())
-    return "";
-  return *max_element(common.begin(), common.end(), size_less());
+  iBests.insert(iBests.begin(), lMax); // Prepend the substrings' length to the array of offsets into a.
+  //cout << "iBests "; for (auto i: iBests) cout << i << " "; cout << "\n";
+  return iBests;
 }
 
 string strFromFile(const char* fileName) {
@@ -107,21 +70,204 @@ vector<string> strsFromFile(const char* filename) {
   return v;
 }
 
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) { return !std::isspace(ch); }));
+}
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) { return !std::isspace(ch); }).base(), s.end());
+}
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+  ltrim(s);
+  rtrim(s);
+}
+
+#if 0
+#include <unicode/unistr.h>
+#include <unicode/ustream.h>
+#include <unicode/locid.h>
+#include <iostream>
+// Converting to lowercase via icu::UnicodeString::toLower("de_DE")
+// is a fool's errand, because it needs conversion to Embarcadero's AnsiString
+// and thence to std::string or char*.  Dependency hell.
+#endif
+
+map<string, string> pronsFromFile(const char* filename, char delimiter = '\t') {
+  map<string, string> m;
+  istringstream ss(strFromFile(filename));
+  string line;
+  while (getline(ss, line, '\n')) {
+    // Convert to lower case.  May crash for non-ASCII multibyte UTF8.
+    transform(line.begin(), line.end(), line.begin(), [](unsigned char c){ return tolower(c); });
+    // Omit words that begin with a nonletter.
+    const auto firstchar = line[0];
+    if (firstchar < 'a' || firstchar > 'z')
+      continue;
+    // Split at delimiter into word and pronunciation.
+    string word, pron;
+    istringstream iss(line);
+    getline(iss, word, delimiter);
+    getline(iss, pron); // omit delimiter?
+    //cout << word << "\t\t---\t\t" << pron << "\n";;;;
+    trim(pron);
+    m[word] = pron;
+  }
+  //;;;; tidy, cull, uniq, like lcs-kinyar.rb:72-92.
+  return m;
+}
+
+// Split STDIN into lines, and each line into spaces.
+vector<vector<string> > strsFromSTDIN() {
+  vector<vector<string> > v;
+  string line;
+  while (getline(cin, line, '\n')) {
+    istringstream iss(line);
+    vector<string> tokens;
+    string s;
+    while (getline(iss, s, ' '))
+      tokens.emplace_back(s);
+    v.emplace_back(tokens);
+  }
+  return v;
+}
+
+// Does this string match the regex /[^_][^_]/ ?
+// Hand-coded, because std::regex requires gcc 4.9, which is too new for Ubuntu 14.04.5.
+// So find "not a _" and another one right thereafter.
+bool onlySingleLettersLeft(const string& s) {
+  const auto n = s.size();
+  if (n < 2)
+    return true;
+  for (auto i=0u; i<n-2; ++i)
+    if (s[i] != '_' && s[i+1] != '_')
+      return false;
+  return true;
+}
+
 int main() {
-  {
-    string s1("t o n z ɛ t ɪ n d ə t ɑ x t ə ɣ ə r j aː r ə m ɛ t m eː ɣ aː ɦ ɪ t s ɑ l s d ə b ɔ m d o r ə s d eː ɛ n ɪ s d");
-    string s2("avondetappe     ɑ v ɔ n d ə t ɑ p ə");
-    cout << "The LCS of '" << s1 << "'\nand '" << s2 << "'\nis '" << lcs(s1, s2) << "'.\n";
+  const auto prondict(pronsFromFile("lcs1/train_all/cmudict-plain.txt"));
+  cout << "Read prondict.\n";
+
+  // Read transcriptions made of nonsense English words.
+  const auto scrips = strsFromSTDIN();
+  cout << "Read scrips.\n";
+
+  // Apply pronunciations to each scrip, i.e. to "cougar aortic thrown" after NI1-2018-07-02_01_051.
+  vector<pair<string,string> > scripsPron;
+  for (auto scrip: scrips) {
+    const string uttid(scrip[0]);
+    scrip.erase(scrip.begin()); // Remove uttid.
+    string phones;
+    for (auto word: scrip)
+      phones += prondict.at(word) + ' '; // .at() handles const map, unlike [].
+    trim(phones);
+    scripsPron.emplace_back(uttid, phones);
   }
-  {
-    auto s1("AH D AH M AH T");
-    auto s2(strsFromFile("lcs1/train_all/cmudict-plain.txt"));
-    cout << "Read prondict.\n";
-    auto s3(s2[999]);
-    cout << "Finding an LCS for '" << s3 << "'.\n";
-    cout << "The LCS is '" << lcs(s1, s3) << "'.\n";
-    cout << "Finding a fancy LCS.\n";
-    cout << "Fancy LCS is '" << lcs(s1, s2) << "'.\n";
+  cout << "Pronounced scrips.\n";
+
+  const auto prondictKinyar(pronsFromFile("kinyar-lexicon.txt", ' '));
+  // abantu, aa b aa n t uw
+
+  // To optimize, cull prondictKinyar.
+
+  // Remap phones to single chars (in prondict and scrips) to avoid matching only part of a multichar phone like 'ao'.\n"; exit(1);
+
+  typedef vector<string> MySet;
+  unordered_map<string, MySet> h; // Map each pron to a collection of homonym words.
+  unordered_map<string, string> lookup; // Map each word to its pron.
+  for (auto& kv: prondictKinyar) {
+    const auto& word = kv.first;
+    const auto& pron = kv.second;
+    //cout << "word " << word << " has pron " << pron << ".\n";
+    auto& value = h[pron]; // Create empty MySet in h, if h doesn't already have "pron."
+    value.emplace_back(word);
+    lookup[word] = pron;
   }
-    return 0;
+  cout << "Prondict had " << prondictKinyar.size() << " pronunciations.\n";
+  // Now lookup has all the pronunciations, and h has all the homonyms.
+  // ;;;; Use lookup.  Use h.
+
+  for (auto scrip: scripsPron) {
+    const auto& uttid(scrip.first);
+    auto phones(scrip.second);
+    cout << uttid << "\t";
+    if (phones.empty()) {
+      cout << "\n";
+      continue;
+    }
+    // lcs-kinyar.rb line 141
+    typedef pair<int, string> AccPair;
+    vector<AccPair> acc;
+    // Keep matching words until phones has only isolated single letters left.
+    while (!onlySingleLettersLeft(phones)) {
+      cout << "\n" << phones << "\n";;;;
+      string vbest;
+      string vword;
+      auto lenBest = 0;
+      vector<pair< vector<string>, string>> bests;
+      cout << "\nscanning prondict...\n";
+      for (auto& kv: prondictKinyar) {
+	const auto& word = kv.first;
+	const auto& pron = kv.second;
+	const auto rgLCS = lcs(phones, pron, lenBest);
+	const auto len = rgLCS[0];
+	const auto n = rgLCS.size() - 1;
+	//cout << "len " << len << "    n " << n << "     w&p: " << word << " , " << pron << "\n";
+	vector<string> substrs; // std::set isn't needed, because rgLCS lacks duplicates.
+	for (auto i=1u; i<=n; ++i)
+	  substrs.emplace_back(phones.substr(rgLCS[i], len));
+	if (len > lenBest) {
+	  lenBest = len;
+	  bests = {{substrs, word}};
+	  cout << "best " << lenBest << ", " << word << " , " << pron << "\n";
+	}
+	else if (len == lenBest) {
+	  bests.emplace_back(substrs, word);
+	}
+      }
+      //cout << "scanned.\n";
+      // lcs-kinyar.rb line 197
+      string closest, closestphonestring;
+      for (auto& ab: bests) {
+	const auto& substr = ab.first;
+	const auto& word = ab.second;
+	closest = word;
+	closestphonestring = *substr.begin();
+	//cout << "Grabbing first not closest: " << closest << " = " << closestphonestring << "\n";;;; // instead use vbest,vword.
+	break;;;;
+      }
+      const auto chosenWord = closest;
+      const auto chosenPhonestring = closestphonestring;
+      // Re-find its phone string in phones.
+      const auto i = phones.find(chosenPhonestring);
+      //cout << "Mark from " << i << " to " << i+chosenPhonestring.size() << "\n";
+      acc.emplace_back(i, chosenWord);
+      for (auto j = i; j < i+chosenPhonestring.size(); ++j)
+	phones[j] = '_';
+    }
+    cout << "Only isolated single letters left.\n";
+
+    // Sort acc by i.  Print its words.
+    sort(acc.begin(), acc.end(),
+      [](const AccPair& lhs, const AccPair& rhs) { return lhs.first < rhs.first; } );
+    for (const auto iw: acc)
+      cout << iw.second << " ";
+    cout << "\n";
   }
+
+  if (0) {
+    const string s1("t o n z ɛ t ɪ n d ə t ɑ x t ə ɣ ə r j aː r ə m ɛ t m eː ɣ aː ɦ ɪ t s ɑ l s d ə b ɔ m d o r ə s d eː ɛ n ɪ s d");
+    const string s2("avondetappe     ɑ v ɔ n d ə t ɑ p ə");
+    const auto r = lcs(s1, s2);
+    cout << "The LCS of '" << s1 << "'\nand '" << s2 << "'\nis '" << s1.substr(r[1], r[0]) << "', of " << r.size()-1 << " total.\n";
+  }
+  if (0) {
+    const string s1("AH D AH M AH T");
+    const auto s2(strsFromFile("lcs1/train_all/cmudict-plain.txt")[990]);
+    const auto r = lcs(s1, s2);
+    cout << "The LCS of '" << s1 << "'\nand '" << s2 << "'\nis '" << s1.substr(r[1], r[0]) << "', of " << r.size()-1 << " total.\n";
+  }
+  return 0;
+}
