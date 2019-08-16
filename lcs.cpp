32487e751908e2d0fcac9d44d00487f39c3ff437
Camille Goudeseune <cog@illinois.edu>
2019-08-16 12:43:24 -0500

    faster matrix; refs; tidy

--- lcs.cpp
+++ lcs.cpp
@@ -170,38 +170,32 @@ int levenshtein(const string& source, const string& target) {
   const int m = target.length();
   if (n == 0) return m;
   if (m == 0) return n;
-  typedef vector<vector<int>> Tmatrix; 
-  Tmatrix matrix(n+1);
-
-  // Size the vectors in the 2.nd dimension. Unfortunately C++ doesn't
-  // allow for allocation on declaration of 2.nd dimension of vec of vec
-  for (int i = 0; i <= n; i++) matrix[i].resize(m+1);
-  for (int i = 0; i <= n; i++) matrix[i][0]=i;
-  for (int j = 0; j <= m; j++) matrix[0][j]=j;
+  vector<vector<int>> M(n+1, vector<int>(m+1));
+  for (int i = 0; i <= n; i++) M[i][0]=i;
+  for (int j = 0; j <= m; j++) M[0][j]=j;
   for (int i = 1; i <= n; i++) {
     const char& s_i = source[i-1];
     for (int j = 1; j <= m; j++) {
       const char& t_j = target[j-1];
       const int cost = s_i == t_j ? 0 : 1;
-      const int above = matrix[i-1][j];
-      const int left = matrix[i][j-1];
-      const int diag = matrix[i-1][j-1];
+      const int& above = M[i-1][j];
+      const int& left  = M[i][j-1];
+      const int& diag  = M[i-1][j-1];
       int cell = min(above + 1, min(left + 1, diag + cost));
-      // Cover transposition, in addition to deletion,
-      // insertion and substitution. This step is taken from:
-      // Berghel, Hal ; Roach, David : "An Extension of Ukkonen's 
-      // Enhanced Dynamic Programming ASM Algorithm"
-      // (http://www.acm.org/~hlb/publications/asm/asm.html)
+      // Cover transposition, as well as deletion, insertion and substitution.
+      // From Berghel & Roach, "An Extension of Ukkonen's Enhanced Dynamic Programming ASM Algorithm,"
+      // www.acm.org/~hlb/publications/asm/asm.html
       if (i>2 && j>2) {
-        int trans=matrix[i-2][j-2]+1;
-        if (source[i-2]!=t_j) trans++;
-        if (s_i!=target[j-2]) trans++;
-        if (cell>trans) cell=trans;
+        int trans = M[i-2][j-2] + 1;
+        if (source[i-2] != t_j) ++trans;
+        if (s_i != target[j-2]) ++trans;
+        if (cell>trans)
+	  cell=trans;
       }
-      matrix[i][j]=cell;
+      M[i][j]=cell;
     }
   }
-  return matrix[n][m];
+  return M[n][m];
 }
 
 int main() {
